56fec4a262cabc11f333da0b2cfb257bdc030fcb
Mady-Lun <mady.lun22@kit.edu.kh>
2025-01-19 15:47:50 +0700

    first commit

--- src/app/contact/map-section.tsx
+++ src/app/contact/map-section.tsx
@@ -11,11 +11,10 @@ export default function MapSection() {
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3908.772901087454!2d104.88!3d11.55!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTHCsDMzJzAwLjAiTiAxMDTCsDUyJzQ4LjAiRQ!5e0!3m2!1sen!2s!4v1639580759201!5m2!1sen!2s"
               width="100%"
               height="100%"
-              style={{ border: 0 }}
               allowFullScreen
-              loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"
-              className="rounded-lg"
+              className="rounded-lg border-0"
+              title="Google Map Location"
             ></iframe>
           </div>
           
